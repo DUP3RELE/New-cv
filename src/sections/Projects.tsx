d8dfabce80edca2ff04198c958b029eb9206a38e
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-09-02 17:25:12 +0200

    working on projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,8 +1,45 @@
+import React, { useRef, useEffect } from "react";
+import "../App.css";
+
 export default function Projects(): any {
+	const cardsContainerRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleMouseMove = (e: MouseEvent) => {
+			if (cardsContainerRef.current) {
+				const cards = Array.from(
+					cardsContainerRef.current.getElementsByClassName("card")
+				) as HTMLElement[];
+
+				cards.forEach((card) => {
+					const rect = card.getBoundingClientRect();
+					const x = e.clientX - rect.left;
+					const y = e.clientY - rect.top;
+
+					card.style.setProperty("--mouse-x", `${x}px`);
+					card.style.setProperty("--mouse-y", `${y}px`);
+				});
+			}
+		};
+
+		const container = cardsContainerRef.current;
+		container?.addEventListener("mousemove", handleMouseMove);
+
+		return () => {
+			container?.removeEventListener("mousemove", handleMouseMove);
+		};
+	}, []);
+
 	return (
 		<section className='section projects'>
-			<h1 className='secondary'>My Projects</h1>
-			<p className='secondary'>This is the projects section.</p>
+			<div id='cards'>
+				<div className='card'></div>
+				<div className='card'></div>
+				<div className='card'></div>
+				<div className='card'></div>
+				<div className='card'></div>
+				<div className='card'></div>
+			</div>
 		</section>
 	);
 }
