ab7eb2350cf96d3e81a7743d51c966fd4efd028a
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-10-07 16:59:52 +0200

    v 1.0

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,40 @@
+import smileIcon from "../icons/smile.svg";
+
 export default function Footer(): any {
 	return (
 		<footer className='section footer'>
-			<div className="left-half-footer"></div>
-			<div className="right-half-footer"></div>
-			<p className="footer-text">© 2024 Krystian Żywczak. All rights reserved.</p>
+			<div className='left-half-footer'>
+				<div className='footer-link-box'>
+					<a
+						className='footer-links'
+						href='https://www.linkedin.com/in/krystian-%C5%BCywczak-0152b0287/'
+						target='_blank'
+						rel='noreferrer'
+						title='Strona Github'
+					>
+						Linkedin
+					</a>
+					<a
+						className='footer-links'
+						href='https://github.com/DUP3RELE?tab=overview&from=2024-09-01&to=2024-09-17'
+						target='_blank'
+						rel='noreferrer'
+						title='Strona Github'
+					>
+						Github
+					</a>
+				</div>
+			</div>
+			<div className='right-half-footer'>
+				<img
+					src={smileIcon}
+					alt='smile icon'
+					className='smileIcon'
+				/>
+			</div>
+			<p className='footer-text'>
+				© 2024 Krystian Żywczak. All rights reserved.
+			</p>
 		</footer>
 	);
 }
